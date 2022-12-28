3f20a30d384167d1d051b95f12ff00a0e9d4e282
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-12-28 09:32:21 -0500

    Update streamlit_app.py
    the variable with the user's input added to the base URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,11 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon") #separate the base URL
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #separate the base URL
 #streamlit.text(fruityvice_response.json()) #Just writes the data to the screen
 
 #Converts the json version of the response in the table format
